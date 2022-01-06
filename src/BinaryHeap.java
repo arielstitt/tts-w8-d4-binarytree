c60d49949c1d9489a9c0b2ed8d18738913c4b69b
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2022-01-06 09:50:31 -0500

    added inary heap logic, wtf

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -4,8 +4,72 @@ import java.util.List;
 public class BinaryHeap {
     List<Integer> heap;
 
-    BinaryHeap(){
+    BinaryHeap() {
         heap = new ArrayList<>();
         heap.add(null);
+    }
+
+    public void Minadd(int value) {
+        if (heap.size() > 1) {
+            int parentIndex = Math.floorDiv(heap.size(), 2);
+            int parentValue = heap.get(parentIndex);
+            if (value >= parentValue) {
+                heap.add(value);
+            } else {
+                heap.add(parentValue);
+                if (parentIndex == 1) {
+                    heap.set(1, value);
+                } else {
+                    int newParentIndex = Math.floorDiv(parentIndex, 2);
+                    int newParentValue = heap.get(newParentIndex);
+                    do {
+                        if (newParentIndex == 1) {
+                            break;
+                        }
+                        heap.set(parentIndex, newParentValue);
+                        parentIndex = newParentIndex;
+                        parentValue = newParentValue;
+                        newParentIndex = Math.floorDiv(parentIndex, 2);
+                        newParentValue = heap.get(newParentIndex);
+                    } while (value < newParentValue);
+                    heap.set(parentIndex, newParentValue);
+                    heap.set(newParentIndex, value);
+                }
+            }
+        } else {
+            heap.add(value);
+        }
+    }
+
+    public void Maxadd(int value) {
+        if (heap.size() > 1) {
+            int parentIndex = Math.floorDiv(heap.size(), 2);
+            int parentValue = heap.get(parentIndex);
+            if (value <= parentValue) {
+                heap.add(value);
+            } else {
+                heap.add(parentValue);
+                if (parentIndex == 1) {
+                    heap.set(1, value);
+                } else {
+                    int newParentIndex = Math.floorDiv(parentIndex, 2);
+                    int newParentValue = heap.get(newParentIndex);
+                    do {
+                        if (newParentIndex == 1) {
+                            break;
+                        }
+                        heap.set(parentIndex, newParentValue);
+                        parentIndex = newParentIndex;
+                        parentValue = newParentValue;
+                        newParentIndex = Math.floorDiv(parentIndex, 2);
+                        newParentValue = heap.get(newParentIndex);
+                    } while (value > newParentValue);
+                    heap.set(parentIndex, newParentValue);
+                    heap.set(newParentIndex, value);
+                }
+            }
+        } else {
+            heap.add(value);
+        }
     }
 }
