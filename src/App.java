42a28086ad1d14df42f26fda06035a3e028f9fc2
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2022-01-06 09:33:03 -0500

    added binary heap exercise

--- src/App.java
+++ src/App.java
@@ -1,13 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BinaryTree bt = new BinaryTree();
-        //no cap
-        int[] nodes = {7, 12, 4, 8, 6, 13, 14, 78, 24, 2};
+        // // BINARY TREE
+        // BinaryTree bt = new BinaryTree();
+        // // no cap
+        // int[] nodes = { 7, 12, 4, 8, 6, 13, 14, 78, 24, 2 };
 
-        for(int n : nodes){
-            bt.add(n);
+        // for (int n : nodes) {
+        //     bt.add(n);
+        // }
+
+        // System.out.println(bt);
+
+        // BINARY HEAP
+        BinaryHeap bh = new BinaryHeap();
+
+        int[] Nodes = { 1, 12, 3, 24, 7, 19, 1, 50, 16 };
+
+        for(int n : Nodes){
+        bh.heap.add(n);
         }
 
-        System.out.println(bt);
+        System.out.println(bh.heap);
     }
+
 }
---
+++ src/BinaryHeap.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryHeap {
+    List<Integer> heap;
+
+    BinaryHeap(){
+        heap = new ArrayList<>();
+        heap.add(null);
+    }
+}
